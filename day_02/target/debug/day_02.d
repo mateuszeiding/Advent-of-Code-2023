95a95c10e1a29fcf429ff021b34f4c4cf86db223
mateuszeiding <49956984+mateuszeiding@users.noreply.github.com>
2023-12-17 23:23:46 +0100

    Delete day_02/target directory

--- day_02/target/debug/day_02.d
+++
@@ -1,1 +0,0 @@
-C:\Repos\Advent\ of\ Code\ 2023\day_02\target\debug\day_02.exe: C:\Repos\Advent\ of\ Code\ 2023\day_02\src\main.rs
--- day_02/target/debug/deps/day_02.d
+++
@@ -1,5 +0,0 @@
-C:\Repos\Advent of Code 2023\day_02\target\debug\deps\day_02.exe: src\main.rs
-
-C:\Repos\Advent of Code 2023\day_02\target\debug\deps\day_02.d: src\main.rs
-
-src\main.rs:
